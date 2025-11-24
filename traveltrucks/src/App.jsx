193928a506eb3108611ccf0ed034ec23912db841
Habibe Çınar <habibecinar07@gmail.com>
2025-11-24 14:49:51 +0300

    header

--- traveltrucks/src/App.jsx
+++ traveltrucks/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <AppRouter />
     </BrowserRouter>
   );
