7aef7ce07f43aba73924b062a9de75623b061022
Habibe Çınar <habibecinar07@gmail.com>
2025-11-25 11:40:34 +0300

    completed home

--- traveltrucks/src/pages/Home/Home.jsx
+++ traveltrucks/src/pages/Home/Home.jsx
@@ -1,5 +1,17 @@
-export default function Home() {
+import "./Home.css";
+
+
+const Home = () => {
   return (
-    <div>Home Page</div>
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Campers of your dreams</h1>
+        <p>You can find everything you want in our catalog</p>
+        <button className="hero-btn">View Now</button>
+      </div>
+
+    </section>
   );
-}
+};
+
+export default Home;
